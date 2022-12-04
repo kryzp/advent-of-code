851edd423bfa096ac2693de679175c525eaa1f08
kryzp <kprzeczek4@gmail.com>
2022-12-04 05:53:15 +0000

    Added Day04.

---
+++ 2022/Day04/solver_part1.py
@@ -0,0 +1,25 @@
+
+f = open("input.txt")
+all_lines = f.readlines()
+
+def get_tasks(dat):
+    parts2 = dat.split("-")
+    return [*range(int(parts2[0]), int(parts2[1]) + 1)]
+
+def contains(a, b):
+    for element in a:
+        if element not in b:
+            return False
+    return True
+
+total = 0
+
+for raw in all_lines:
+    line = raw.strip()
+    parts = line.split(",")
+    p1 = get_tasks(parts[0])
+    p2 = get_tasks(parts[1])
+    if contains(p1, p2) or contains(p2, p1):
+        total += 1
+
+print(total)
