4903fba1a2a92ccc08e37ff69a8c4c61ef4f2d92
kryzp <kprzeczek4@gmail.com>
2022-12-23 17:38:55 +0000

    Realized Day21 P2 could be done way easier.

--- 2022/Day21/part2.py
+++ 2022/Day21/part2.py
@@ -5,29 +5,27 @@ finished = set()
 monkeys = {}
 monkey_values = {}
 
-equation = ""
-
 def apply(name):
 	global monkeys
 	operation = monkeys[name]
 	split = operation.split(" ")
 	if name == "humn":
-		return "(-1j)"
+		return -1j
 	if len(split) == 1:
-		return "(" + split[0] + ")"
+		return int(split[0])
 	lhs, operator, rhs = split
 	lhsval = apply(lhs)
 	rhsval = apply(rhs)
 	if name == "root":
-		return lhsval + "-" + rhsval
+		return lhsval - rhsval
 	if operator == '+':
-		return "(" + lhsval + "+" + rhsval + ")"
+		return lhsval + rhsval
 	elif operator == '-':
-		return "(" + lhsval + "-" + rhsval + ")"
+		return lhsval - rhsval
 	elif operator == '*':
-		return "(" + lhsval + "*" + rhsval + ")"
+		return lhsval * rhsval
 	elif operator == '/':
-		return "(" + lhsval + "/" + rhsval + ")"
+		return lhsval / rhsval
 
 for l in ll:
 	split = l.split(": ")
@@ -35,11 +33,5 @@ for l in ll:
 	op = split[1]
 	monkeys.update({name: op})
 
-equation = apply("root")
-print(equation)
-
-evaled = eval(equation)
-print(evaled)
-result = evaled.real / evaled.imag
-
-print("root value = " + str(result))
+out = apply("root")
+print("root value = " + str(out.real / out.imag))
