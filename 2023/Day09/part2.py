5efeb7ef6e15b3c7a1a8dccbd054d7af6ee3d623
kryzp <kprzeczek4@gmail.com>
2023-12-09 13:13:15 +0000

    Day09

---
+++ 2023/Day09/part2.py
@@ -0,0 +1,23 @@
+
+ll = [x for x in open("input.txt").read().strip().split("\n")]
+
+x = 0
+
+def solve(data):
+	constant = True
+	for i in range(len(data) - 1):
+		if data[i] != data[i + 1]:
+			constant = False
+			break
+	if constant:
+		return data[0]
+	deltas = []
+	for i in range(len(data) - 1):
+		deltas.append(data[i + 1] - data[i])
+	return data[0] - solve(deltas)
+
+for l in ll:
+	data = [int(n) for n in l.split(" ")]
+	x += solve(data)
+
+print(x)
